71f6ab33f5ea33c3799756badb0523e2dfaae3de
pitsolu <pitsolu@gmail.com>
2016-09-07 03:53:50 +0300

    memory leak solved.. for sure now, removed jquery ui and tableHeadFixer, changed README.md, create native plugins i.e resizeColumns, freezeHeader, freezeLeftColumn, thanks a bunch to tableHeadFixer author

--- js/src/script.js
+++ js/src/script.js
@@ -1,94 +1,5 @@
 (function($){
 
-	var jsonData = [
-
-		{
-			"id":1,
-			"firstname":"Samuel",
-			"lastname":"Weru",
-			"status":"Pending",
-			"mobile":"N/A",
-			"address":"Koleni",
-			"email":"samweru@gmail.com",
-			"employed":"No",
-			"married":"No",
-			"county":"Nakuru"
-		},
-		{
-			"id":2,
-			"firstname":"Burney",
-			"lastname":"Rubble",
-			"status":"Active",
-			// "mobile":"07228899200",
-			"mobile":"0722889920072288992007228899200722889920072288992007228899200722889920072288992007228899200722889920",
-			"address":"Bedrock",
-			"email":"b.rubble@gmail.com",
-			"employed":"Yes",
-			"married":"Yes",
-			"county":"Fairy Tale Land"
-		},
-		{
-			"id":3,
-			"firstname":"Fred",
-			"lastname":"Flistone",
-			"status":"Pending",
-			"mobile":"0770234567",
-			"address":"Bedrock",
-			"email":"fred.stone@gmail.com",
-			"employed":"Yes",
-			"married":"Yes",
-			"county":"Fairy Tale Land"
-		},
-		{
-			"id":4,
-			"firstname":"James",
-			"lastname":"Bond",
-			"status":"Pending",
-			"mobile":"0772223344",
-			"address":"London",
-			"email":"007@gmail.com",
-			"employed":"Yes",
-			"married":"No",
-			"county":"Winchester"
-		},
-		{
-			"id":5,
-			"firstname":"Clark",
-			"lastname":"Kent",
-			"status":"Active",
-			"mobile":"0778889982",
-			"address":"Superland",
-			"email":"clark.kent@dailyplanet.com",
-			"employed":"Yes",
-			"married":"No",
-			"county":"Smallville"
-		},
-		{
-			"id":7,
-			"firstname":"Charles",
-			"lastname":"Spurgeon",
-			"status":"Active",
-			"mobile":"077929293",
-			"address":"London",
-			"email":"charles.spurgeon@thecalvinist.com",
-			"employed":"Yes",
-			"married":"Yes",
-			"county":"Birmingham"
-		},
-		{
-			"id":8,
-			"firstname":"John",
-			"lastname":"Calvin",
-			"status":"Active",
-			"mobile":"0778559982",
-			"address":"Westbrough",
-			"email":"john.calvin@thecalvinist.com",
-			"employed":"Yes",
-			"married":"Yes",
-			"county":"London"
-		}
-	];
-
 	var btnAdd = $(document.createElement("BUTTON"));
 	btnAdd.html("Add");
 
@@ -106,11 +17,14 @@
 		title:"Sample Grid",
 		usePager:true,
 		// data:jsonData,
-		singleSelect:false,
-		// columnHide:[
+		singleSelect:true,
+		// freezeHeader:false,
+		// freezeLeftColumn:false,
+		// resizeColumns:false,
+		columnHide:[
 
-		// 	"id"
-		// ],
+			"id"
+		],
 		toolbars:[
 
 			[btnAdd, btnSel]
