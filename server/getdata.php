753cc72fdb0b6390afeac4439ced35f2d606b3b1
samweru <samweru@gmail.com>
2020-10-13 21:45:46 +0300

    getdata.php to work with cli or http

--- server/getdata.php
+++ server/getdata.php
@@ -2,17 +2,38 @@
 
 $conn = require "connect.php";
 
+if(!empty($_REQUEST)){
+
+	$start_at = $_REQUEST["start_at"];
+	$page_size = $_REQUEST["page_size"];
+}
+
+if(!empty($_SERVER["argv"])){
+
+	$argv = $_SERVER["argv"];
+	$start_at = $argv[1];
+	$page_size = $argv[2];	
+}
+
 try {
 
-  $stmt = $conn->prepare("SELECT * FROM employee");
+  if($start_at > 0)
+  	$start_at = $start_at - 1;
+
+  $stmt = $conn->prepare(sprintf("SELECT * FROM employee ORDER BY id LIMIT %d, %d", 
+  									$start_at, $page_size));
+
   $stmt->execute();
 
   $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
   
-  exit(json_encode($stmt->fetchAll()));
+  exit(json_encode(array(
+
+  	"rows"=>$stmt->fetchAll(), 
+  	"count"=>$page_size
+  )));
 } 
 catch(PDOException $e) {
   
   exit(sprintf("Error: %s", $e->getMessage()));
-} 
-
+}
